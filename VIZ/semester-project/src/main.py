b811316c8db232ff12f3561ca24ef71fb927585a
Ablack-git <andrewzak774@gmail.com>
2020-05-31 15:34:40 +0200

    VIZ: added multiple new visualisations

--- VIZ/semester-project/src/main.py
+++ VIZ/semester-project/src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from pyqtgraph.Qt import QtCore, QtGui
-from src.vis import SharedPlotsTrellis
+from src.vis import SharedPlotsTrellis, MultiplePlotsTrellis, SideBySideTrellis, TrellisWithSlider
 
 batting = pd.read_csv('data/batting.csv')
 player_1: pd.DataFrame = batting.loc[(batting['First Name'] == 'Derek') & (batting['Last Name'] == 'Jeter')]
@@ -10,15 +10,24 @@ p1_name = player_1['First Name'].iloc[0] + ' ' + player_1['Last Name'].iloc[0]
 p2_name = player_2['First Name'].iloc[0] + ' ' + player_2['Last Name'].iloc[0]
 col_names = ('Runs', 'Hits', 'Homeruns', 'BA', 'PA', 'TB', 'SlugPct', 'OBP', 'OPS', 'BABIP')
 
-# for col_name in col_names:
-#     data_y[col_name] = {p1_name: player_1[col_name].to_numpy(), p2_name: player_2[col_name].to_numpy()}
-
-app = QtGui.QApplication([])
-plot = SharedPlotsTrellis("Demo")
 data = {p1_name: player_1, p2_name: player_2}
-plot.plot('Year', col_names, **data)
-plot.add_legend()
-plot.show()
+app = QtGui.QApplication([])
+# plot = SharedPlotsTrellis("Demo")
+# plot.plot('Year', col_names, **data)
+# plot.add_legend()
+# plot.show()
+#
+# plot2 = MultiplePlotsTrellis(title='Demo2')
+# plot2.plot('Year', col_names, **data)
+# plot2.show()
+#
+# plot3 = SideBySideTrellis(title='Demo3')
+# plot3.plot('Year', col_names, scale='same_axis', **data)
+# plot3.show()
+
+plot3 = TrellisWithSlider.create_trellis_with_slider('Year', col_names, **data)
+plot3.show()
+
 if __name__ == '__main__':
     import sys
 
